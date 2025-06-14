ea2784906d117fdc35f021f04a1a40903f358d06
Albert-louis2004 <emt2025ro@gmail.com>
2025-06-15 00:35:40 +0300

    Delete src directory
    Stergere directoare din cadrul fostului proiect

--- src/main/java/org/example/inchiriazausor/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.inchiriazausor.repository;
-
-import org.example.inchiriazausor.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
-} 
